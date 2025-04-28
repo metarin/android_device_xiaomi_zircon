c27588ab83549515d0a84e70d5bab6d1af58cdb6
metarin <mail@metarin.net>
2025-04-28 19:42:33 +0900

    zircon: Shim NFC libs with libbase_shim

--- extract-files.py
+++ extract-files.py
@@ -81,6 +81,8 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/vendor.mediatek.hardware.pq_aidl-V1-ndk.so', 'vendor/lib/vendor.mediatek.hardware.pq_aidl-V2-ndk.so', 'vendor/lib/vendor.mediatek.hardware.pq_aidl-V3-ndk.so',
     'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V1-ndk.so', 'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so', 'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V3-ndk.so'): blob_fixup()
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+    ('odm/lib64/nfc_nci.thn31nfc.tms.so', 'odm/lib64/nfc_nci.nqx.default.hw.so', 'odm/lib64/tms-utils.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
